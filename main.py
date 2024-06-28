70d4641af4899dfdfb6e457cd49f99ca492d96d4
phbonnot <phbonnot@gmail.com>
2024-06-28 16:45:24 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,19 +6,25 @@ Created on Tue Apr  9 14:43:02 2024
 @author: bonnot
 """
 
-import configparser
 import requests
 import json
 import requetes
 from recap_jury_API import recap_jury_API
 from documentLatex import documentLatex
+from connexion_API import connexion_API
+
 
 """permet de retrouver la liste des ids de tous les semetres de l'année
 à insérer en paramètre dans la requête concernant la décision de jury"""
-def formsemestres_ids(data):
+"""def formsemestres_ids(data):
     ids=[]
     for i in range(len(data)):
         ids.append(data[i]["formsemestre_id"])
+    return ids"""
+def formsemestres_ids(data):
+    ids=[]
+    for i,sem in data:
+        ids.append(sem["formsemestre_id"])
     return ids
 
 def id_semestre_pair(data):
@@ -30,7 +36,7 @@ def id_semestre_pair(data):
     return ids_sem_pair
 
 """On se connecte"""
-config = configparser.ConfigParser()
+"""config = configparser.ConfigParser()
 print("lecture de la configuration")
 config.read('config.ini')
 
@@ -41,17 +47,15 @@ response = requests.post(config['server']['Base_Url']+"api/tokens", auth = (conf
                                                                                                                 
 token = response.json()['token']
 print("token : ",token)
-header = {"Authorization" : "Bearer " + token}
+header = {"Authorization" : "Bearer " + token}"""
+
+connexion = connexion_API()
 
 annee=2022
 
 requete = requetes.formsemestresAnnee(annee)
 
-response = requests.get(config['server']['Base_Url']+ requete, headers = header, verify = False)
-#data = json.loads(response.content)
-#for i in range(len(data)):
-#    print(data[i]['date_debut_iso']," ",data[i]['date_fin_iso']," ",data[i]['titre_num'],data[i]['formsemestre_id'])
-#print(json.dumps(data,indent=4))
+response = requests.get(connexion.get_config()['server']['Base_Url']+ requete, headers = connexion.get_header(), verify = False)
 
 if not response:
     print("Erreur : La chaîne JSON est vide.")
@@ -68,20 +72,30 @@ else:
         print(nb_comptes_rendus)
         for i in range(nb_comptes_rendus):
             print("i : ",i)
-            requete2 = requetes.decisionJury(data_sem_pair_ids[i])
-            response2 = requests.get(config['server']['Base_Url']+ requete2, headers = header, verify = False)
-            requeteEtudiant = requetes.etudiantsInscritsDans(data_sem_pair_ids[i])
-            response3 = requests.get(config['server']['Base_Url']+ requeteEtudiant, headers = header, verify = False)
-            if not response2 or not response3:
+            requete_decision = requetes.decisionJury(data_sem_pair_ids[i])
+            response_decision = requests.get(connexion.get_config()['server']['Base_Url']+ requete_decision, headers = connexion.get_header(), verify = False)
+            requete_etudiant = requetes.etudiantsInscritsDans(data_sem_pair_ids[i])
+            response_etudiant = requests.get(connexion.get_config()['server']['Base_Url']+ requete_etudiant, headers = connexion.get_header(), verify = False)
+           
+            if not response_decision or not response_etudiant:
                 print("Erreur : La chaîne JSON est vide.")
             else:
                 try:
-                    data2 = json.loads(response2.content)
-                    fichier_etudiants = json.loads(response3.content)
-                    recap_jury = recap_jury_API(annee,(i+1),data2,fichier_etudiants)
-                    #compteRendu = documentLatex("année 2022 BUT"+str(i+1))
-                    #compteRendu.compte_rendu_jury(recap_jury.tableauValidationRCUEs())
-                    print(json.dumps(recap_jury.repartition_bacs()))
+                    data_decision = json.loads(response_decision.content)
+                    fichier_etudiants = json.loads(response_etudiant.content)
+                    
+                    for etudiant in fichier_etudiants:
+                        code_nip = etudiant["code_nip"]
+                        for etudiant_2 in data_decision:
+                            if code_nip == etudiant_2["code_nip"]:
+                                etudiant_2["nom"]=etudiant["nom"]
+                                etudiant_2["prenom"]=etudiant["prenom"]
+                                etudiant_2["civilite"]=etudiant["civilite"]
+                                etudiant_2["bac"]=etudiant["admission"]["bac"]
+                                etudiant_2["annee_du_bac"]=etudiant["admission"]["annee"]
+                    recap_jury = recap_jury_API(annee,(i+1),data_decision,fichier_etudiants)
+                    recap_jury.tableauValidationRCUEs()
+                    print(recap_jury.tableau_stats())
                 except json.JSONDecodeError as er:
                     print(f"Erreur de décodage JSON : {er}")
     except json.JSONDecodeError as e:
--- recap_jury_API.py
+++ recap_jury_API.py
@@ -17,8 +17,9 @@ class recap_jury_API:
     def __init__(self,annee,but,data,fichier_etudiants):
         self.annee=annee
         self.but=but
-        self.data = data
-        self.fichier_etudiants = fichier_etudiants
+        self.data_decisions = data
+        self.fichier_etudiants = fichier_etudiants#la liste des étudiants inscrits dans l'année
+        #self.renseignements_etudiants = renseignements_etudiants#code_nip, nom,prénom,sexe,type_bac et année du bac
         self.dict_etudiants_code_nip_nom ={}
         for etudiant in self.fichier_etudiants:
             self.dict_etudiants_code_nip_nom[etudiant['code_nip']]=etudiant['sort_key']   
@@ -26,35 +27,45 @@ class recap_jury_API:
             self.nbUEs=3
         else:
             self.nbUEs=6
-        self.tab_stats = np.zeros((3,4))
-        self.longueurTab_stats = 4
-        self.tabRCUEs = []
+        self.tab_stats = np.zeros((5,7))
+        self.longueurTab_stats = 5
+        self.types_de_bac_gene = ["S","Général","Géné(RéoL1,L2)","G\u00c9N\u00c9","GENE(REOR L1)"]
+        self.dict_types_de_bac = {"géné":[],"techno":[],"autre":[]}
+        self.tab_admis=[]
         
         
     def afficher_data(self):
         print(json.dumps(self.data,indent=4))
         
     def tableauValidationRCUEs(self):
+        tabRCUEs = []
         for i in range(self.nbUEs+1):
-            self.tabRCUEs.append([])
+            tabRCUEs.append([])
             
-        for etudiant in range(len(self.data)):
+        for ind_etudiant,etudiant in enumerate(self.data_decisions):
             resultEtudiant=[]
-            code_etudiant=self.data[etudiant]['code_nip']
+            code_etudiant=self.data_decisions[ind_etudiant]['code_nip']
             if code_etudiant in self.dict_etudiants_code_nip_nom:
                 nom_etudiant = self.dict_etudiants_code_nip_nom[code_etudiant].split(';')[0]
                 prenom_etudiant = self.dict_etudiants_code_nip_nom[code_etudiant].split(';')[1]
-                resultEtudiant.append(self.data[etudiant]['code_nip'])
+                resultEtudiant.append(self.data_decisions[ind_etudiant]['code_nip'])
                 resultEtudiant.append(nom_etudiant)
                 resultEtudiant.append(prenom_etudiant)
                 nbRcues = 0
-                for j in range(self.nbUEs):
-                    if self.data[etudiant]['rcues'][j]['moy'] >= 10:
+                nbRcues_entre_huit_et_dix =0
+                for j in range(self.nbUEs):#on parcourt toutes les ues
+                    if self.data_decisions[ind_etudiant]['rcues'][j]['moy'] >= 10:
                         nbRcues = nbRcues + 1
-                    resultEtudiant.append(round(self.data[etudiant]['rcues'][j]['moy'],2))
-            #resultEtudiant.append(nbRcues)
-                self.tabRCUEs[nbRcues].append(resultEtudiant)
-        return self.tabRCUEs   
+                    elif self.data_decisions[ind_etudiant]['rcues'][j]['moy'] >= 8:
+                        nbRcues_entre_huit_et_dix = nbRcues_entre_huit_et_dix +1
+                    resultEtudiant.append(round(self.data_decisions[ind_etudiant]['rcues'][j]['moy'],2))
+                tabRCUEs[nbRcues].append(resultEtudiant)
+                if (nbRcues + nbRcues_entre_huit_et_dix) == self.nbUEs:
+                    self.tab_admis.append(resultEtudiant)
+                    etudiant["ADM"]=True
+                else:
+                    etudiant["ADM"]=False
+        return tabRCUEs
 
     def repartition_homme_femme(self):
         dict={"M":[],"F":[]}
@@ -66,15 +77,56 @@ class recap_jury_API:
         return dict
     
     def repartition_bacs(self):
-        dict = {"gene":[],"Techno":[],"Autre":[]}
         for etudiant in self.fichier_etudiants:
-            if etudiant["admission"]["bac"] in ["S","Général", "Géné(RéoL1,L2)","L","G\u00c9N\u00c9","GENE(REOR L1)"]:
-                dict["gene"].append(etudiant['nom'])
+            if etudiant["admission"]["bac"] in self.types_de_bac_gene:
+                self.dict_types_de_bac["géné"].append(etudiant['nom']+","+etudiant['prenom'])
             elif etudiant["admission"]["bac"] == "STMG" or etudiant["admission"]["bac"] == "STI2D":
-                dict['Techno'].append(etudiant['nom'])
+                self.dict_types_de_bac['techno'].append(etudiant['nom']+","+etudiant['prenom'])
             else:
-                dict['Autre'].append(etudiant['nom'])
-        return dict
+                self.dict_types_de_bac['autre'].append(etudiant['nom']+","+etudiant['prenom'])
+        return self.dict_types_de_bac
             
     
+    def repartition_neo_reor(self):
+        dict={"neo":[],"reor":[]}
+        for etudiant in self.renseignements_etudiants:
+            if etudiant in self.fichier_etudiants and etudiant["annee_du_bac"]==self.annee:
+                dict["neo"].append(etudiant)
+            else:
+                dict["reor"].append(etudiant)
+        return dict
+            
+    def get_admis(self):
+        return self.tab_admis
+    
+    def tableau_stats(self):
+        ligne = 0
+        colonne = 0
+        adm = 0
+        for etudiant in self.data_decisions:
+            if etudiant["annee_du_bac"] == self.annee:
+                if etudiant["bac"] == "géné":
+                    ligne = 1
+                elif etudiant["bac"] == "techno":
+                    ligne = 2
+                else:
+                    ligne = 3
+            else:
+                ligne = 4
+            if etudiant["civilite"] == "M":
+                colonne = 3
+                if etudiant["ADM"]:
+                    adm = 4      
+            else:
+                colonne = 1
+                if etudiant["ADM"]:
+                    adm = 2 
+            self.tab_stats[ligne][colonne] +=1
+            self.tab_stats[ligne][adm] +=1
+            
+                
+        return self.tab_stats
+                
+                
+        
   
